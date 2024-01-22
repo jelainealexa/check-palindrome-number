094b43c77a57687c29d584af3523a53ca5befe3b
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-22 20:31:53 +0800

    Pseudocode

--- exercise-9.py
+++ exercise-9.py
@@ -1,1 +1,11 @@
 # Check Palindrome Number
+
+# Check if the given is a palindrome number
+
+# Convert number to string
+
+# Reverse the string
+
+# Compare original and reversed strings
+
+# Print results
