37539f990f6ee4a5fbd3e6fcb5b3f5d334a1f5ce
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-22 20:41:02 +0800

    Check if given are palindromes

--- exercise-9.py
+++ exercise-9.py
@@ -1,17 +1,24 @@
 # Check Palindrome Number
 
-# Check if the given is a palindrome number
 def palindrome(number):
     # Convert number to string
     str_number = str(number)
 
     # Reverse the string
-    reverse_string = str(number[-1])
+    reverse_string = str_number[-1]
 
     # Compare original and reversed strings
     if str_number == reverse_string:
         return True
     else:
         return False
+
+# Given
+number_1 = 505
+number_2 = 325
+
+# Check if given are palindrome numbers
+result_1 = palindrome(number_1)
+result_2 = palindrome(number_2)
 
 # Print results
